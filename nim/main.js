5b0d2262518cf280196836024f506e1970e4d3ee
Max1Truc <maxitruc.bonbec@gmail.com>
2019-11-07 18:38:44 +0100

    classical nim update

--- nim/main.js
+++ nim/main.js
@@ -1,70 +1,98 @@
-function solve(heap) {
-  var xored = 0;
+var HEAP = [];
 
-  heap.forEach(e => {
-    xored ^= e;
-  });
+function genheap(number_of_lines, place_by_line) {
+  let heap = [];
 
-  for (let i = 0; i < heap.length; i++) {
-    let others_xor = xored ^ heap[i];
-    if (others_xor < heap[i]) {
-      return [i, others_xor];
+  for (let i = 0; i < number_of_lines; i++) {
+    let line = [];
+    for (let j = 0; j < place_by_line; j++) {
+      line.push(0);
     }
-  }
-
-  return [];
-}
-
-function getheap() {
-  var lines = Number(prompt("Number of lines ?"));
-  var heap = [];
-
-  for (let i = 1; i <= lines; i++) {
-    var nb = prompt("Remaining in line n°" + i + "?");
-    heap.push(nb);
+    heap.push(line);
   }
 
   return heap;
 }
 
-function showmove(heap, move, result_selector) {
+function count(arr, value) {
+  var c = 0;
+
+  for (e of arr) {
+    if (e == value) {
+      c += 1;
+    }
+  }
+
+  return c;
+}
+
+function replace(arr, original_value, new_value, count, reverse = false) {
+  arr = arr.reverse()
+  return arr.map(e => {
+    if (e == 1 && count > 0) {
+      count -= 1
+      return new_value;
+    }
+    return e;
+  }).reverse();
+}
+
+function solve(heap) {
+  var xored = 0;
+
+  heap.forEach(e => {
+    xored ^= count(e, 1);
+  });
+
+  for (let i = 0; i < heap.length; i++) {
+    var c = count(heap[i], 1)
+    let solution = xored ^ c;
+    if (solution < c) {
+      heap[i] = replace(heap[i], 1, 0, c - solution, true)
+      return heap;
+    }
+  }
+  return heap;
+}
+
+function showheap(heap, result_selector) {
   var result_element = document.querySelector(result_selector);
+  result_element.innerHTML = ""; // Empty the result container
   var table = document.createElement("table");
-  table.setAttribute("class", "centered")
+  table.setAttribute("class", "centered");
 
   for (let i = 0; i < heap.length; i++) {
     var tr = document.createElement("tr");
-    table.appendChild(tr);
 
-    for (let j = 0; j < heap[i]; j++) {
+    for (let j = 0; j < heap[i].length; j++) {
       var td = document.createElement("td"),
         div = document.createElement("div");
 
-      if (move.length > 0 && move[0] == i && j >= move[1]) {
-        div.setAttribute("class", "empty dot");
+      if (heap[i][j] === 1) {
+        div.setAttribute("class", "stick");
       } else {
-        div.setAttribute("class", "dot");
+        div.setAttribute("class", "removed stick");
       }
+
+      div.addEventListener("click", () => {
+        HEAP[i][j] = HEAP[i][j] === 1 ? 0 : 1;
+        showheap(HEAP, result_selector);
+      });
 
       td.appendChild(div);
       tr.appendChild(td);
     }
+
+    table.appendChild(tr);
   }
 
   result_element.appendChild(table);
-
-  if (move.length == 0) {
-    var h4 = document.createElement("h4");
-    h4.innerText = "Do whatever you want, you're in a bad case.";
-    h4.style.color = "red";
-    result_element.appendChild(h4);
-  }
 }
 
-function main() {
-  let heap = getheap();
-  let move = solve(heap);
-  showmove(heap, move, "#result");
-}
+HEAP = genheap(5, 20);
+showheap(HEAP, "#result");
 
-main();
+document.getElementById("solve").addEventListener("click", () => {
+  HEAP = solve(HEAP);
+  showheap(HEAP, "#result");
+});
